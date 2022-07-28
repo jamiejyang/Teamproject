cdfbfc1d76e61281a90a92a27e32179f64473a91
Administrator <Administrator@User -2022NWARS>
2022-07-28 09:55:10 +0900

    멍

--- 2nd/src/Test.java
+++ 2nd/src/Test.java
@@ -5,6 +5,8 @@ public class Test {
 		System.out.println("응애응애 송아지 ");
 		System.out.println("누가 나를 위로해주지. 바로 여러분");
 		System.out.println("안녕하세용");
+		String junsik = "살아있다";
+		System.out.println(junsik);
 	}
 
 }
